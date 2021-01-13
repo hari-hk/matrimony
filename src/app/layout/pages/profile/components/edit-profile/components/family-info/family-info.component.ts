cbf51ba5b638d89a80a0d336ea5f970768c88c34
hari-hk <hariharakumar5196@gmail.com>
2021-01-13 23:17:40 +0530

    family info view completed

--- src/app/layout/pages/profile/components/edit-profile/components/family-info/family-info.component.ts
+++ src/app/layout/pages/profile/components/edit-profile/components/family-info/family-info.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
 import { Subscription } from 'rxjs';
+import { AutoCompleteComponent } from 'src/app/common/components/auto-complete/auto-complete.component';
+import { Masters } from 'src/app/masters/masters';
+import { Occupation } from 'src/app/masters/occupation.master';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
@@ -14,11 +18,21 @@ export class FamilyInfoComponent implements OnInit, OnDestroy {
 
   subscriptions: Array<Subscription> = [];
 
+
+  familyType: Array<any> = new Masters().familyType;
+  familyStatus: Array<any> = new Masters().familyStatus;
+  occupation = new Occupation().occupation.map((el: any) => {
+    el.id = el.name;
+    return el;
+  });
+  motherOccupation: Array<any> = new Masters().motherOccupation;
+
   formOnEdit = false;
 
   constructor(
     private fb: FormBuilder,
-    private userService: UserService) { }
+    private userService: UserService,
+    private dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.initFamilyForm();
@@ -45,8 +59,19 @@ export class FamilyInfoComponent implements OnInit, OnDestroy {
       if (!response) {
         return;
       }
-      // this.setBasicForm(response);
+      console.log(response);
+
+      this.setFormValue(response);
     }));
+  }
+  setFormValue(data): void {
+    this.fif.familyType.setValue(data.familyType);
+    this.fif.familyStatus.setValue(data.familyStatus);
+    this.fif.fatherOccupation.setValue(data.fatherOccupation);
+    this.fif.motherOccupation.setValue(data.motherOccupation);
+    this.fif.brothers.setValue(data.noofBrothers);
+    this.fif.sisters.setValue(data.noofSisters);
+    this.fif.nativePlace.setValue(data.nativePlace);
   }
   togleFormMode(): void {
     this.formOnEdit = !this.formOnEdit;
@@ -57,4 +82,33 @@ export class FamilyInfoComponent implements OnInit, OnDestroy {
   get fif(): any {
     return this.familyInfoForm.controls;
   }
+  openAutoComplete(form, control): void {
+    const dialogRef = this.dialog.open(AutoCompleteComponent, {
+      width: '320px',
+      data: this.getPopUpTitles(control),
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        console.log(result);
+        this[form].controls[control].setValue(result);
+      }
+    });
+  }
+  getPopUpTitles(control): any {
+    const data: any = {
+      title: '',
+      list: [],
+      multiple: false
+    };
+    switch (control) {
+      case 'fatherOccupation':
+        data.title = 'Select Occupation';
+        data.list = this.occupation;
+        break;
+      default:
+        break;
+    }
+    return data;
+  }
+
 }
